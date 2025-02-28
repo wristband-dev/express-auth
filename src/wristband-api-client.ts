56a9d57c16772c845358e5a99d7f5ab6cc5dc894
KK <kkarski@appcrossings.com>
2025-02-28 11:05:17 +0000

    fixed overwrite

--- src/wristband-api-client.ts
+++ src/wristband-api-client.ts
@@ -1,11 +1,33 @@
 import axios, { AxiosInstance } from 'axios';
+import http from 'http';
+import https from 'https';
+
+import { FORM_URLENCODED_MEDIA_TYPE, JSON_MEDIA_TYPE } from './utils/constants';
 
 export class WristbandApiClient {
-  readonly axiosInstance: AxiosInstance;
+  public axiosInstance: AxiosInstance;
 
   constructor(wristbandApplicationDomain: string) {
     this.axiosInstance = axios.create({
       baseURL: `https://${wristbandApplicationDomain}/api/v1`,
+      httpAgent: new http.Agent({
+        keepAlive: true,
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        timeout: 60000,
+        keepAliveMsecs: 1000,
+        scheduling: 'lifo',
+      }),
+      httpsAgent: new https.Agent({
+        keepAlive: true,
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        timeout: 60000,
+        keepAliveMsecs: 1000,
+        scheduling: 'lifo',
+      }),
+      headers: { 'Content-Type': FORM_URLENCODED_MEDIA_TYPE, Accept: JSON_MEDIA_TYPE },
+      maxRedirects: 0,
     });
   }
 }
